3d98ea2358ebd1a7d9a5fa54ee0c60c7d39c4abb
Khusi2005 <139037790+Khusi2005@users.noreply.github.com>
2025-04-09 10:09:45 +0530

    Update triggers.sql

--- SQL_Scripts/triggers.sql
+++ SQL_Scripts/triggers.sql
@@ -1,16 +1,34 @@
 use project_5;
 
--- 4. Update stock after a sale.
+-- 4. Update stock after a sale. 
 
 delimiter $$  
 create trigger update_stock_after_sale  
 after insert on Sales  
 for each row  
 begin  
-update Product  
-set Stock=Stock-new.QuantitySold  
-where ProductID=new.ProductID;  
+	update Product  
+	set Stock=Stock-new.QuantitySold  
+	where ProductID=new.ProductID;  
 end$$  
 delimiter ; 
 
 insert into Sales(SaleID,ProductID,QuantitySold,SaleDate) values (219,11,2,'2025-04-03');
+
+-- 7. Remove a discontinued product from the inventory 
+
+ delimiter $$
+ create trigger after_supplier_delete
+ before delete on Suppliers
+ for each row
+ begin
+	 delete from Sales
+	 where ProductID in(
+	 select ProductID from Product where SupplierID=old.SupplierID
+	  );
+	 delete from Product
+	 where SupplierID=old.SupplierID;
+ end $$
+ delimiter ;
+ delete from Suppliers
+ where SupplierID=107;
